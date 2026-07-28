acdc8bb42f5509a085d71f8601f010eca24a667c
Mykyta Lazovskyi <j.lazovskyi@gmail.com>
2026-07-28 21:48:50 +0300

    widgets were  collapsed from 12 amount to 6 only;  optimization;

--- m5-device/main.py
+++ m5-device/main.py
@@ -20,9 +20,9 @@ class AppConfig:
         self.wifi_ssid = "empty"
         self.wifi_pass = "empty"
         self.update_rate = 0.5
-        self.mqtt_broker_host="192.168.31.169",
-        self.mqtt_broker_port="1883",
-        self.desktop_topic="pc/data",
+        self.mqtt_broker_host="192.168.31.169"
+        self.mqtt_broker_port="1883"
+        self.desktop_topic="pc/data"
         self.orangepi5_topic="opi5/data"
 
     def load_from_file(self, filename):
@@ -76,8 +76,7 @@ def connect_wifi(conf: AppConfig):
 # =========================
 
 
-HIGHLIGHTED_NAV_TEXT_COLOR = 0x00ff55
-HIGHLIGHTED_NAV_BACKGROUND_COLOR = 0x00AA55
+HIGHLIGHTED_NAV_TEXT_COLOR = 0xFFFFFF
 BACKGROUND_COLOR = 0x222222
 NAV_BACKGROUND_COLOR = 0x00AAAA
 NAV_TEXT_COLOR = 0x00ffff
@@ -85,26 +84,26 @@ TITLE_TEXT_COLOR = 0xAAAAAA
 VALUE_TEXT_COLOR = 0xFFFFFF
 
 
-WIDGETS_INITIAL_X = 15
+WIDGETS_INITIAL_X = 10
 WIDGETS_INITIAL_Y = 20
 NAVBAR_INITIAL_X = 30
 NAVBAR_INITIAL_Y = 200
 
-WIDGET_OFFSET_X = 100
-WIDGET_OFFSET_Y = 100
+WIDGET_OFFSET_X = 150
+WIDGET_OFFSET_Y = 60
 VALUE_OFFSET_Y = 35
 
 TITLE_TEXT_SIZE = 1.5
 VALUE_TEXT_SIZE = 1.2
 
-MAX_COLUMNS = 3
+MAX_COLUMNS = 2
 
 
 pcTitles = ["CPU", "RAM", "TEMP", "GPU", "VRAM", "TEMP"]
 opi5Titles = ["CPU", "RAM", "TEMP", "DOWN", "SSD", "ZRAM"]
 buttons = ["opi5", "tst", "pc"]
 
-widgets = []
+# widgets = []
 valueLabels = []
 navbar  = []
 
@@ -123,9 +122,6 @@ def button_a_handler(state):
     dehighlight(0)
     mqtt.subscribe(cfg.orangepi5_topic)
     mqtt.unsubscribe(cfg.desktop_topic)
-    widgets[3].setText(opi5Titles[3])
-    widgets[4].setText(opi5Titles[4])
-    widgets[5].setText(opi5Titles[5])
 
 def button_b_handler(state):
     highligh(1)
@@ -136,9 +132,6 @@ def button_c_handler(state):
     dehighlight(2)
     mqtt.unsubscribe(cfg.orangepi5_topic)
     mqtt.subscribe(cfg.desktop_topic)
-    widgets[3].setText(pcTitles[3])
-    widgets[4].setText(pcTitles[4])
-    widgets[5].setText(pcTitles[5])
 
 def buildWidgets(titles: list[str]):
     x_multiplier = 0
@@ -152,11 +145,11 @@ def buildWidgets(titles: list[str]):
         x = WIDGETS_INITIAL_X + x_multiplier * WIDGET_OFFSET_X
         y = WIDGETS_INITIAL_Y + y_multiplier * WIDGET_OFFSET_Y
 
-        widgets.append(
+        valueLabels.append(
             Widgets.Label(title, x, y, TITLE_TEXT_SIZE, TITLE_TEXT_COLOR, BACKGROUND_COLOR, Widgets.FONTS.DejaVu18))
 
-        valueLabels.append(
-            Widgets.Label("0", x, y + VALUE_OFFSET_Y, VALUE_TEXT_SIZE, VALUE_TEXT_COLOR, BACKGROUND_COLOR, Widgets.FONTS.DejaVu24))
+        # valueLabels.append(
+        #     Widgets.Label("0", x, y + VALUE_OFFSET_Y, VALUE_TEXT_SIZE, VALUE_TEXT_COLOR, BACKGROUND_COLOR, Widgets.FONTS.DejaVu24))
 
         x_multiplier += 1
 
@@ -181,23 +174,23 @@ def parse_kv(s: str) -> dict:
 def on_message(topic, msg):
     if topic == MQTT_PC_TOPIC.encode('utf-8'):
         dict = parse_kv(msg)
-        valueLabels[0].setText(dict["cpu"])      # CPU
-        valueLabels[1].setText(dict["ram"])      # RAM
-        valueLabels[2].setText(dict["temp_cpu"]) # TEMP CPU
+        valueLabels[0].setText(pcTitles[0] + ": " + dict["cpu"])      # CPU
+        valueLabels[1].setText(pcTitles[1] + ": "  + dict["ram"])      # RAM
+        valueLabels[2].setText(pcTitles[2] + ": "  + dict["temp_cpu"]) # TEMP CPU
 
-        valueLabels[3].setText(dict["gpu"])      # GPU
-        valueLabels[4].setText(dict["vram"])     # VRAM
-        valueLabels[5].setText(dict["temp_gpu"]) # TEMP GPU
+        valueLabels[3].setText(pcTitles[3] + ": "  + dict["gpu"])      # GPU
+        valueLabels[4].setText(pcTitles[4] + ": "  + dict["vram"])     # VRAM
+        valueLabels[5].setText(pcTitles[5] + ": "  + dict["temp_gpu"]) # TEMP GPU
 
     if topic == MQTT_OPI_TOPIC.encode('utf-8'):
         dict = parse_kv(msg)
-        valueLabels[0].setText(dict["cpu"])      # CPU
-        valueLabels[1].setText(dict["ram"])      # RAM
-        valueLabels[2].setText(dict["temp_cpu"]) # TEMP CPU
+        valueLabels[0].setText(opi5Titles[0] + ": "  + dict["cpu"])      # CPU
+        valueLabels[1].setText(opi5Titles[1] + ": "  + dict["ram"])      # RAM
+        valueLabels[2].setText(opi5Titles[2] + ": "  + dict["temp_cpu"]) # TEMP CPU
 
-        valueLabels[3].setText(dict["net_spd"])      # GPU
-        valueLabels[4].setText(dict["ssd"])     # VRAM
-        valueLabels[5].setText(dict["zram"]) # TEMP GPU
+        valueLabels[3].setText(opi5Titles[3] + ": "  + dict["net_spd"])      # GPU
+        valueLabels[4].setText(opi5Titles[4] + ": "  + dict["ssd"])     # VRAM
+        valueLabels[5].setText(opi5Titles[5] + ": "  + dict["zram"]) # TEMP GPU
 
 def connect_mqtt(conf: AppConfig):
     print("SSID:", conf.wifi_ssid)
