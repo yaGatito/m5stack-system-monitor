348ea1e390fb731b24b1fed651c426531ff67009
Mykyta Lazovskyi <j.lazovskyi@gmail.com>
2026-07-29 04:59:26 +0300

    added themes;

--- m5-device/main.py
+++ m5-device/main.py
@@ -74,45 +74,96 @@ def connect_wifi(conf: AppConfig):
 # UI
 # =========================
 
+IDX_HIGHLIGHTED_NAV_TEXT_COLOR = 0
+IDX_HIGHLIGHTED_NAV_BACKGROUND_COLOR = 1
+IDX_BACKGROUND_COLOR = 2
+IDX_NAV_BACKGROUND_COLOR = 3
+IDX_NAV_TEXT_COLOR = 4
+IDX_TITLE_TEXT_COLOR = 5
+IDX_VALUE_TEXT_COLOR = 6
 
-HIGHLIGHTED_NAV_TEXT_COLOR = 0xFFFFFF
-BACKGROUND_COLOR = 0x222222
-# NAV_BACKGROUND_COLOR = 0x00AAAA
-NAV_BACKGROUND_COLOR = 0x111111
-NAV_TEXT_COLOR = 0x666666
-TITLE_TEXT_COLOR = 0xAAAAAA
-VALUE_TEXT_COLOR = 0xFFFFFF
+themes = [
+    #[navbar] [navback]backg    nav back navcolor titlclr  valcolor
+    [0xFFFFFF,0xFFFFFF,0x333333,0x222222,0x666666,0xAAAAAA,0xFFFFFF], # GREY THEME
+    [0xFFFFFF,0xFFFFFF,0x1E2430,0x151A22,0x64748B,0x94A3B8,0xFFFFFF], # DARK BLUE THEME
+    [0xFFFFFF,0xFFFFFF,0x1C2521,0x121816,0x60756B,0x9AAFA5,0xFFFFFF], # DARK GREEN THEME
+    [0xFFFFFF,0xFFFFFF,0x211F2B,0x16151D,0x706A80,0xAAA4B8,0xFFFFFF], # CYBERPUNK THEME
+    [0xFFFFFF,0xFFFFFF,0x292722,0x1C1A17,0x756F63,0xADA79B,0xFFFFFF], # AMBER / INDUSTRIAL
+    [0x000000,0x00FF41,0x050805,0x0A120A,0x287A3D,0x5DBB72,0xD7FFD9], # MATRIX
+    [0x282A36,0xBD93F9,0x282A36,0x1E1F29,0x6272A4,0x8BE9FD,0xF8F8F2], # DRACULA
+    [0x2E3440,0x88C0D0,0x2E3440,0x242933,0x616E88,0xD8DEE9,0xECEFF4], # NORD
+    [0x001014,0x00E5FF,0x07181C,0x0B252B,0x39727C,0x76B9C2,0xD8FBFF], # CYAN TERMINAL
+    [0xFFFFFF,0xA855F7,0x17121F,0x0F0B14,0x6E5A7E,0xBFA8D1,0xFFFFFF], # PURPLE NEON
+    [0xFFFFFF,0xF97316,0x241A17,0x17110F,0x80675D,0xC9A99B,0xFFF7F2], # SUNSET
+    [0xFFFFFF,0x0EA5E9,0x0B1F2A,0x07151D,0x4D8299,0x8EC5DC,0xF0FAFF], # OCEAN
+    [0xFFFFFF,0x16A34A,0x101C15,0x0A120D,0x4C7659,0x9AC2A4,0xF0FFF3], # FOREST
+    [0x1A1200,0xFFB000,0x0F0C05,0x181207,0x80652A,0xC69D4D,0xFFE9A8], # AMBER TERMINAL
+    [0xFFFFFF,0xDC2626,0x1F1214,0x140A0C,0x7D4B52,0xC38B91,0xFFF5F5], # BLOOD MOON
+    [0x102027,0x67E8F9,0x15252B,0x0C171B,0x52727A,0xA5D8E0,0xECFEFF], # ICE
+    [0xFFF7ED,0xA16207,0x29211D,0x1C1512,0x78675D,0xC4B5AA,0xFFF7ED], # COFFEE
+    [0xFFFFFF,0x2563EB,0xF3F4F6,0xE5E7EB,0x6B7280,0x374151,0x111827], # LIGHT
+    [0x000000,0xFFFFFF,0x000000,0x080808,0x555555,0x888888,0xFFFFFF], # MONO OLED
+]
+themePointer = 0
 
 
-WIDGETS_INITIAL_X = 10
+WIDGETS_INITIAL_X = 15
 WIDGETS_INITIAL_Y = 20
 NAVBAR_INITIAL_X = 30
 NAVBAR_INITIAL_Y = 200
 
-WIDGET_OFFSET_X = 150
-WIDGET_OFFSET_Y = 60
+WIDGET_OFFSET_X = 100
+WIDGET_OFFSET_Y = 90
 VALUE_OFFSET_Y = 35
 
 TITLE_TEXT_SIZE = 1.5
 VALUE_TEXT_SIZE = 1.2
 
-MAX_COLUMNS = 2
+MAX_COLUMNS = 3
 
 
 pcTitles = ["CPU", "RAM", "TEMP", "GPU", "VRAM", "TEMP"]
 opi5Titles = ["CPU", "RAM", "TEMP", "DOWN", "SSD", "ZRAM"]
-buttons = ["opi5", "tst", "pc"]
+buttons = ["OPI5", "CLR", "PC"]
 
-# widgets = []
+widgets = []
 valueLabels = []
 navbar  = []
 
 OPI5_MODE = 0
-TEST_MODE = 1
+CLR_MODE = 1
 PC_MODE = 2
 
 current_mode = 1
 previous_mode = 0
+
+
+def updateColors():
+    global themePointer
+    if themePointer >= len(themes):
+        themePointer = 0
+    else:
+        themePointer = themePointer + 1
+
+    Widgets.fillScreen(themes[themePointer][IDX_BACKGROUND_COLOR])
+
+    widgets[0].setColor(themes[themePointer][IDX_TITLE_TEXT_COLOR], themes[themePointer][IDX_BACKGROUND_COLOR])
+    widgets[1].setColor(themes[themePointer][IDX_TITLE_TEXT_COLOR], themes[themePointer][IDX_BACKGROUND_COLOR])
+    widgets[2].setColor(themes[themePointer][IDX_TITLE_TEXT_COLOR], themes[themePointer][IDX_BACKGROUND_COLOR])
+    widgets[3].setColor(themes[themePointer][IDX_TITLE_TEXT_COLOR], themes[themePointer][IDX_BACKGROUND_COLOR])
+    widgets[4].setColor(themes[themePointer][IDX_TITLE_TEXT_COLOR], themes[themePointer][IDX_BACKGROUND_COLOR])
+    widgets[5].setColor(themes[themePointer][IDX_TITLE_TEXT_COLOR], themes[themePointer][IDX_BACKGROUND_COLOR])
+
+    valueLabels[0].setColor(themes[themePointer][IDX_VALUE_TEXT_COLOR], themes[themePointer][IDX_BACKGROUND_COLOR])
+    valueLabels[1].setColor(themes[themePointer][IDX_VALUE_TEXT_COLOR], themes[themePointer][IDX_BACKGROUND_COLOR])
+    valueLabels[2].setColor(themes[themePointer][IDX_VALUE_TEXT_COLOR], themes[themePointer][IDX_BACKGROUND_COLOR])
+    valueLabels[3].setColor(themes[themePointer][IDX_VALUE_TEXT_COLOR], themes[themePointer][IDX_BACKGROUND_COLOR])
+    valueLabels[4].setColor(themes[themePointer][IDX_VALUE_TEXT_COLOR], themes[themePointer][IDX_BACKGROUND_COLOR])
+    valueLabels[5].setColor(themes[themePointer][IDX_VALUE_TEXT_COLOR], themes[themePointer][IDX_BACKGROUND_COLOR])
+
+    navbar[0].setColor(themes[themePointer][IDX_NAV_TEXT_COLOR])
+    navbar[1].setColor(themes[themePointer][IDX_HIGHLIGHTED_NAV_TEXT_COLOR])
+    navbar[2].setColor(themes[themePointer][IDX_NAV_TEXT_COLOR])
 
 def highligh(idx: int):
     global current_mode
@@ -121,14 +172,15 @@ def highligh(idx: int):
         return
     previous_mode = current_mode
     current_mode = idx
-    navbar[previous_mode].setColor(NAV_TEXT_COLOR)
-    navbar[current_mode].setColor(HIGHLIGHTED_NAV_TEXT_COLOR)
+    navbar[previous_mode].setColor(themes[themePointer][IDX_NAV_TEXT_COLOR], themes[themePointer][IDX_NAV_BACKGROUND_COLOR])
+    navbar[current_mode].setColor(themes[themePointer][IDX_HIGHLIGHTED_NAV_TEXT_COLOR], themes[themePointer][IDX_HIGHLIGHTED_NAV_BACKGROUND_COLOR])
 
 def button_a_handler(state):
     highligh(OPI5_MODE)
 
 def button_b_handler(state):
-    highligh(TEST_MODE)
+    highligh(CLR_MODE)
+    updateColors()
 
 def button_c_handler(state):
     highligh(PC_MODE)
@@ -145,17 +197,17 @@ def buildWidgets(titles: list[str]):
         x = WIDGETS_INITIAL_X + x_multiplier * WIDGET_OFFSET_X
         y = WIDGETS_INITIAL_Y + y_multiplier * WIDGET_OFFSET_Y
 
-        valueLabels.append(
-            Widgets.Label(title, x, y, TITLE_TEXT_SIZE, TITLE_TEXT_COLOR, BACKGROUND_COLOR, Widgets.FONTS.DejaVu18))
+        widgets.append(
+            Widgets.Label(title, x, y, TITLE_TEXT_SIZE, themes[themePointer][IDX_TITLE_TEXT_COLOR], themes[themePointer][IDX_BACKGROUND_COLOR], Widgets.FONTS.DejaVu18))
 
-        # valueLabels.append(
-        #     Widgets.Label("0", x, y + VALUE_OFFSET_Y, VALUE_TEXT_SIZE, VALUE_TEXT_COLOR, BACKGROUND_COLOR, Widgets.FONTS.DejaVu24))
+        valueLabels.append(
+            Widgets.Label("0", x, y + VALUE_OFFSET_Y, VALUE_TEXT_SIZE, themes[themePointer][IDX_VALUE_TEXT_COLOR], themes[themePointer][IDX_BACKGROUND_COLOR], Widgets.FONTS.DejaVu24))
 
         x_multiplier += 1
 
-    navbar.append(Widgets.Label(buttons[0], 30, 200, TITLE_TEXT_SIZE, NAV_TEXT_COLOR, NAV_BACKGROUND_COLOR, Widgets.FONTS.DejaVu24))
-    navbar.append(Widgets.Label(buttons[1], 135, 200, TITLE_TEXT_SIZE, NAV_TEXT_COLOR, NAV_BACKGROUND_COLOR, Widgets.FONTS.DejaVu24))
-    navbar.append(Widgets.Label(buttons[2], 233, 200, TITLE_TEXT_SIZE, NAV_TEXT_COLOR, NAV_BACKGROUND_COLOR, Widgets.FONTS.DejaVu24))
+    navbar.append(Widgets.Label(buttons[0], 30, 210, VALUE_TEXT_SIZE, themes[themePointer][IDX_NAV_TEXT_COLOR], themes[themePointer][IDX_NAV_BACKGROUND_COLOR], Widgets.FONTS.DejaVu24))
+    navbar.append(Widgets.Label(buttons[1], 135, 210, VALUE_TEXT_SIZE, themes[themePointer][IDX_NAV_TEXT_COLOR], themes[themePointer][IDX_NAV_BACKGROUND_COLOR], Widgets.FONTS.DejaVu24))
+    navbar.append(Widgets.Label(buttons[2], 233, 210, VALUE_TEXT_SIZE, themes[themePointer][IDX_NAV_TEXT_COLOR], themes[themePointer][IDX_NAV_BACKGROUND_COLOR], Widgets.FONTS.DejaVu24))
 
 
 # =========================
@@ -176,25 +228,33 @@ def on_message(topic, msg):
         if current_mode != PC_MODE:
             return
         dict = parse_kv(msg)
-        valueLabels[0].setText(pcTitles[0] + ": " + dict["cpu"])        # CPU
-        valueLabels[1].setText(pcTitles[1] + ": "  + dict["ram"])       # RAM
-        valueLabels[2].setText(pcTitles[2] + ": "  + dict["temp_cpu"])  # TEMP CPU
+        widgets[3].setText(pcTitles[3]) # title GPU
+        widgets[4].setText(pcTitles[4]) # title VRAM
+        widgets[5].setText(pcTitles[5]) # title TEMP GPU
 
-        valueLabels[3].setText(pcTitles[3] + ": "  + dict["gpu"])       # GPU
-        valueLabels[4].setText(pcTitles[4] + ": "  + dict["vram"])      # VRAM
-        valueLabels[5].setText(pcTitles[5] + ": "  + dict["temp_gpu"])  # TEMP GPU
+        valueLabels[0].setText(dict["cpu"])       # CPU
+        valueLabels[1].setText(dict["ram"])       # RAM
+        valueLabels[2].setText(dict["temp_cpu"])  # TEMP CPU
+
+        valueLabels[3].setText(dict["gpu"])       # GPU
+        valueLabels[4].setText(dict["vram"])      # VRAM
+        valueLabels[5].setText(dict["temp_gpu"])  # TEMP GPU
 
     if topic == MQTT_OPI_TOPIC.encode('utf-8'):
         if current_mode != OPI5_MODE:
             return
         dict = parse_kv(msg)
-        valueLabels[0].setText(opi5Titles[0] + ": "  + dict["cpu"])     # CPU
-        valueLabels[1].setText(opi5Titles[1] + ": "  + dict["ram"])     # RAM
-        valueLabels[2].setText(opi5Titles[2] + ": "  + dict["temp_cpu"])# TEMP CPU
+        widgets[3].setText(opi5Titles[3])   # title DOWN SPD
+        widgets[4].setText(opi5Titles[4])   # title SSD
+        widgets[5].setText(opi5Titles[5])   # title ZRAM
 
-        valueLabels[3].setText(opi5Titles[3] + ": "  + dict["net_spd"]) # GPU
-        valueLabels[4].setText(opi5Titles[4] + ": "  + dict["ssd"])     # VRAM
-        valueLabels[5].setText(opi5Titles[5] + ": "  + dict["zram"])    # TEMP GPU
+        valueLabels[0].setText(dict["cpu"])      # CPU
+        valueLabels[1].setText(dict["ram"])      # RAM
+        valueLabels[2].setText(dict["temp_cpu"]) # TEMP CPU
+
+        valueLabels[3].setText(dict["net_spd"]) # GPU
+        valueLabels[4].setText(dict["ssd"])     # VRAM
+        valueLabels[5].setText(dict["zram"])    # TEMP GPU
 
 def connect_mqtt(conf: AppConfig):
     print("SSID:", conf.wifi_ssid)
@@ -219,7 +279,6 @@ def connect_mqtt(conf: AppConfig):
 # MOSI = GPIO23
 # CS   = GPIO4
 # freq = 1 MHz
-
 
 def setup():
     M5.begin()
@@ -254,7 +313,7 @@ def setup():
     M5.Display.setCursor(10,10)
     M5.Display.print("MQTT CONNECTED")
 
-    Widgets.fillScreen(BACKGROUND_COLOR)
+    Widgets.fillScreen(themes[themePointer][IDX_BACKGROUND_COLOR])
     current_mode = OPI5_MODE
     buildWidgets(opi5Titles)
     highligh(OPI5_MODE)
